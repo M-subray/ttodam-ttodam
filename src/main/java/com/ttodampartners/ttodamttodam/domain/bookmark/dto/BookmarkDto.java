511ee20248e9cde4ac8ab701262d46d9582225af
Yang ye lin <oky07031217@gmail.com>
2024-04-29 23:20:39 +0900

    merge

--- src/main/java/com/ttodampartners/ttodamttodam/domain/bookmark/dto/BookmarkDto.java
+++ src/main/java/com/ttodampartners/ttodamttodam/domain/bookmark/dto/BookmarkDto.java
@@ -2,7 +2,10 @@ package com.ttodampartners.ttodamttodam.domain.bookmark.dto;
 
 import com.ttodampartners.ttodamttodam.domain.bookmark.entity.BookmarkEntity;
 import com.ttodampartners.ttodamttodam.domain.post.dto.ProductListDto;
+import com.ttodampartners.ttodamttodam.domain.post.entity.PostEntity;
 import lombok.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,8 +16,14 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookmarkDto {
+    private Long bookmarkId;
     private Long userId;
     private Long postId;
+    private Long authorId;
+    private String authorNickname;
+    private PostEntity.Category category;
+    private PostEntity.Status status;
+    private PostEntity.PurchaseStatus purchaseStatus;
     private String postTitle;
     private String postStatus;
     private List<ProductListDto> products;
@@ -27,8 +36,14 @@ public class BookmarkDto {
                 .collect(Collectors.toList());
 
         return BookmarkDto.builder()
-                .userId(bookmarkEntity.getUser().getId())
+                .bookmarkId(bookmarkEntity.getBookmarkId())
                 .postId(bookmarkEntity.getPost().getPostId())
+                .userId(bookmarkEntity.getUser().getId())
+                .authorId(bookmarkEntity.getPost().getUser().getId())
+                .authorNickname(bookmarkEntity.getPost().getUser().getNickname())
+                .category(bookmarkEntity.getPost().getCategory())
+                .status(bookmarkEntity.getPost().getStatus())
+                .purchaseStatus(bookmarkEntity.getPost().getPurchaseStatus())
                 .postTitle(bookmarkEntity.getPost().getTitle())
                 .postStatus(postStatus)
                 .products(products)
